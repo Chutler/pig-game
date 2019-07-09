50adb71928a9b58ad6a2a5848ff4895b211aba4e
Craig Hutler and Carrie Schmid <carrie@>
2019-07-08 20:56:27 -0700

    add JS to create random number and set up two players

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,32 @@
 
 function diceRoll() {
   var randomRoll = Math.floor(Math.random() * 6) +1;
-  document.getElementById("roll").innerHTML = randomRoll
+  document.getElementById("roll1").innerHTML = randomRoll;
+  turnResult(randomRoll);
+}
+
+var playerOne = []
+var playerTwo = []
+
+function turnResult(randomRoll) {
+  if (randomRoll > 1) {
+    playerOne.push(randomRoll);
+  } else {
+    print("End of turn"); // wrong print method
+    console.log(playerOne); // return sum of array to a 3rd div
+    $('#playerOneButton').hide();
+    $('#playerTwoButton').show();
+    // function turnResult2();
+  }
+}
+
+function turnResult2(randomRoll) {
+  if (randomRoll > 1) {
+    playerTwo.push(randomRoll);
+  } else {
+    print("End of turn"); // wrong print method
+    console.log(playerOne);
+  }
 }
 
 // Interface in the front
@@ -12,7 +37,3 @@ function diceRoll() {
 // $("#dice").click(function() {
 //   console.log(Math.floor(Math.random() * 6) +1);
 // });
-
-$("#dice").click(function() {
-  console.log(random);
-});
