fe8a4ea53541faef1a8bf31da3edeba4e5338012
Craig Hutler <craig@craighutler.com>
2019-07-13 12:17:51 -0700

    updated index file and scripts. Still working to init objects to start game.

--- js/scripts.js
+++ js/scripts.js
@@ -29,39 +29,54 @@ Player.prototype.Hold = function() {
   }
 };
 
-  // Monday's functions for each Player 1 turn
-function turnResult(randomRoll) {
-  if (randomRoll > 1) {
-    playerOne.push(randomRoll);
-  } else {
-    alert("End of turn"); // wrong print method
-    console.log(playerOne); // return sum of array to a 3rd div
-    $('#playerOneButton').hide();
-    $('#playerTwoButton').show();
-    // function turnResult2();
-  }
+// Need to create funtion to check active player and show that players buttons while hiding inactive
+
+// Monday's functions for each Player 1 turn
+// holding on this for nostalgic reasons 
+
+// function turnResult(randomRoll) {
+//   if (randomRoll > 1) {
+//     playerOne.push(randomRoll);
+//   } else {
+//     alert("End of turn"); // wrong print method
+//     console.log(playerOne); // return sum of array to a 3rd div
+//     $('#playerOneButton').hide();
+//     $('#playerTwoButton').show();
+//     // function turnResult2();
+//   }
+
+
 
 // Interface in the front
 
 $(document).ready(function() {
+  debugger;
+  $(":button").click(function() {
+    var player1 = new Player();
+    var player2 = new Player(); 
+    debugger;
+  });
+  // Using any button click to create the new objects to start the game.  
+  // However the vars appear to be local an unavailable outside of the function.
+  // Not sure how to create var to create object this way and be global...
 
-  var player1 = new Player();
-  var player2 = new Player();
-
-  $('player1-total-score').text(0);
-  $('player2-total-score').text(0);
+  $('#score1').text(0);
+  $('#score2').text(0);
+  // $('#roll').text(5); // test value for styling
 
   $('button#player-roll').click(function() {
     if (isPlayer1Turn) {
       player1.diceRoll();
-      $('#roll').text(player1.currentRoll);
+      $('#roll').text(player1.diceRoll);
       $('#round1').text(player1.turnScore);
     } else {
       player2.diceRoll();
-      $('#roll').text(player2.currentRoll);
+      $('#roll').text(player2.diceRoll);
       $('#round2').text(player2.turnScore);
     }
   });
+
+  
 
   $('button#player-hold').click(function() {
     if (isPlayer1Turn) {
